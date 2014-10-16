f032cf0218d7e01d13ec38199ae53b7aeeb642b7
9CAT <9cat@users.noreply.github.com>
2014-10-16 11:30:49 -0700

    Update networks.js
    update the genius block info.

--- networks.js
+++ networks.js
@@ -4,40 +4,19 @@ var hex = function(hex) {return new Buffer(hex, 'hex');};
 
 exports.livenet = {
   name: 'livenet',
-  addressVersion: 0x00,
-  magic: hex('f9beb4d9'),
+  addressVersion: 0x41,
+  magic: hex('c1c1c1c1'),
   genesisBlock: {
     height: 0,
-    nonce: 2083236893,
+    nonce: 24706485,
     version: 1,
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
+    hash: hex('c741436d354a9fb337fbd79e4b2750732571f00e8b78d04493f9e9283cf4ad0b'),
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    timestamp: 1395705600,
+    merkle_root: hex('deceb268776c85c006c65804eb53e55960a85aaf75e366a5c7b0bf961b6defec'),
     bits: 486604799
   },
-  genesisBlockTx: {
-    outs: [{
-      v: hex('00F2052A01000000'), // 50 BTC
-      s: new Put()
-        .word8(65) // 65 bytes of data follow
-        .put(hex('04678AFDB0FE5548271967F1A67130B7105CD6A828E03909A67962E0EA1F61DEB649F6BC3F4CEF38C4F35504E51EC112DE5C384DF7BA0B8D578A4C702B6BF11D5F'))
-        .word8(0xAC) // OP_CHECKSIG
-        .buffer()
-    }],
-    lock_time: 0,
-    version: 1,
-    hash: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
-    ins: [{
-      q: 0xFFFFFFFF,
-      o: hex("0000000000000000000000000000000000000000000000000000000000000000FFFFFFFF"),
-      s: new Put()
-        .put(hex('04FFFF001D010445'))
-        .put(new Buffer('The Times 03/Jan/2009 Chancellor on brink of ' +
-                        'second bailout for banks', 'ascii'))
-        .buffer()
-    }]
-  },
+ 
   proofOfWorkLimit: hex("00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"),
   checkpoints: [], // need to put checkpoint blocks here
   addressPubkey: 0,
